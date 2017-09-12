fcb3ca02f8db6580017f9a5940a05819b2a2cea0
zhengwei1949 <zhengwei1949@qq.com>
2017-09-12 19:21:31 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,13 @@
 console.log('定时器开始');
+function myTime(callback){
+    setTimeout(()=>{
+        console.log('定时器进行中');
+        endTimer();
+    },1000);    
+}
 function endTimer(){
     console.log('定时器结束');
 }
-setTimeout(()=>{
-    console.log('定时器进行中');
-    endTimer();
-},1000);
+myTime(endTimer);
+
+
